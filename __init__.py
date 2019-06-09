9be72efa3b3f8199711b16206b7da62597de86fd
Yurii Barninets <y.barninets@gmail.com>
2019-06-09 23:57:01 +0300

    Add  __init__.py for root directory

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .graph import Edge
+from .graph import Vertex
+from .graph import Graph
+from .graph import display_graph
--- graph/graph_visualizer.py
+++ graph/graph_visualizer.py
@@ -1,4 +1,4 @@
-from graph import Graph
+from .graph import Graph
 from PIL import Image
 import pydot
 import tempfile
